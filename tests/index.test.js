08d2fd449c49352a9dd39380e568ea43205d5847
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-24 23:50:02 +0100

    tests: update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,25 +1,9 @@
 
 const { default: testFunc } = await import("../");
 
-describe("check for bad input", () => {
-    test("nothing", async () => {
-        expect(testFunc()).toEqual(false);
-    });
-
-    test("boolean", async () => {
-        expect(testFunc(true)).toEqual(false);
-    });
-
-    test("a number", async () => {
-        expect(testFunc(123)).toEqual(false);
-    });
-
-    test("an empty string", async () => {
-        expect(testFunc("")).toEqual(false);
-    });
-
-    test("an invalid string", async () => {
-        expect(testFunc("foo")).toEqual(false);
+describe("invalid input", () => {
+    test("not a hex string", async () => {
+        expect(testFunc("foobar")).toEqual(false);
     });
 });
 
@@ -38,11 +22,14 @@ describe("whole numbers", () => {
 });
 
 describe("floating point numbers", () => {
-    test("hex charset", async () => {
+    test("valid numbers", async () => {
         expect(testFunc("abcd.1234", true)).toEqual(true);
+        expect(testFunc("ace.cafe", true)).toEqual(true);
     });
 
-    test("hex string", async () => {
-        expect(testFunc("ace.cafe", true)).toEqual(true);
+    test("invalid decimal points", async () => {
+        expect(testFunc(".abcd", true)).toEqual(false);
+        expect(testFunc("abcd.", true)).toEqual(false);
+        expect(testFunc("a.b.c.d", true)).toEqual(false);
     });
 });
