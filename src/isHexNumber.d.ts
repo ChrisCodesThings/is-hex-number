98fe02254af18cb608af5f03dc3f20c175f324c9
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-24 23:50:16 +0100

    feat: add JSDoc and TS defs

---
+++ src/isHexNumber.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Test if a string is a hex number
+ * @param {string} str String to test
+ * @param {boolean} [float=false] Allow floating point hex numbers
+ * @returns {boolean}
+ */
+export default function isHexNumber(str: string, float?: boolean): boolean;
--- src/isHexNumber.js
+++ src/isHexNumber.js
@@ -1,9 +1,17 @@
+// @ts-check
+
 import isString from '@chriscodesthings/is-string';
 
-export default function (str, float = false) {
-    if (!float) {
-        return isString(str) && !!str.match(/^[0-9a-f]+$/i);
-    }
+/**
+ * Test if a string is a hex number
+ * @param {string} str String to test
+ * @param {boolean} [float=false] Allow floating point hex numbers
+ * @returns {boolean}
+ */
+export default function isHexNumber(str, float = false) {
+    const rxWhole = /^[0-9a-f]+$/i;
+    const rxFloat = /^[0-9a-f]+(\.[a-z0-9]+)?$/i;
+    const rx = !float ? rxWhole : rxFloat;
 
-    return isString(str) && !!str.match(/^[0-9a-z]+\.?[0-9a-z]*$/i);
+    return isString(str) && !!str.match(rx);
 }
